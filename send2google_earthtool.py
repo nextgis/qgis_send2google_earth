7da570093fd53a4d499c23aa2f18c63f3bd8ad23
Maxim Dubinin <sim@gis-lab.info>
2015-08-31 14:08:52 -0500

    try popen with shift under linux

--- send2google_earthtool.py
+++ send2google_earthtool.py
@@ -105,9 +105,9 @@ class Send2GEtool(QgsMapTool):
       if platform.dist()[0] == 'debian':
         linpath = linpath_debian
       if event.modifiers() == Qt.ShiftModifier:
-        ret = os.fork(linpath + " " + f.name)
+        ret = subprocess.Popen([linpath,f.name]) #ret = os.fork(linpath + " " + f.name)
       else:
-        ret = os.startfile(f.name)
+        ret = os.system(linpath + " " + f.name)
     elif platform.system() == "Darwin":
       ret = os.system("open " + f.name)
     else:
